c61446f53730f6e6f3ece5f1c81c02b1b23e0906
Faryyaz <dilfary@hotmail.com>
2021-03-16 21:21:27 +0400

    Analytics implementation

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -1,9 +1,35 @@
 <template>
-    <div>Analytics</div>
+    <v-card max-height="100%">
+        <v-card-title>Analytics</v-card-title>
+        <v-card-text>
+            <canvas id="canvas"></canvas>
+        </v-card-text>
+    </v-card>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { Chart, ChartData } from 'chart.js'
 
 @Component
-export default class Analytics extends Vue {}
+export default class Analytics extends Vue {
+    element = <HTMLCanvasElement> this.$el.querySelector('#canvas');
+    ctx =  <CanvasRenderingContext2D> this.element.getContext('2d');
+    labels: Array<string> = [];
+    datasets: Array<string> = [];
+
+    mounted() {
+
+    }
+
+    plot() {
+        new Chart(this.ctx, {
+            type: 'line',
+            data: <ChartData> {
+                labels: this.labels,
+                datasets: this.datasets
+            }
+        });
+    }
+
+}
 </script>
