9d6f5df30237722bd8f16cb2ad1c1292a3ce82b4
Faryyaz <dilfary@hotmail.com>
2021-03-17 20:33:08 +0400

    [Completed] analytics

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-card max-height="100%">
+    <v-card min-height="500px">
         <v-card-title>Analytics</v-card-title>
         <v-card-text>
             <canvas class="js-canvas"></canvas>
@@ -9,44 +9,52 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import { Chart, ChartData } from 'chart.js'
+import * as fb from '../firebase'
+import { VuetifyThemeItem } from 'node_modules/vuetify/types/services/theme'
 
 @Component
 export default class Analytics extends Vue {
-    element = this.$el.querySelector('.canvas') as HTMLCanvasElement;
-    ctx = this.element.getContext('2d') as CanvasRenderingContext2D;
+    element!: HTMLCanvasElement;
+    ctx!: CanvasRenderingContext2D;
     labels: Array<string> = [];
+    infoColor: VuetifyThemeItem = this.$vuetify.theme.currentTheme.info;
+    primaryColor: VuetifyThemeItem = this.$vuetify.theme.currentTheme.primary;
+    errorColor: VuetifyThemeItem = this.$vuetify.theme.currentTheme.error;
+
     datasets = {
         cases: {
             label: 'Cases',
-            data: [],
+            data: [] as Array<number>,
             fill: false,
-            borderColor: '',
-            backgroundColor: '',
+            borderColor: this.infoColor,
+            backgroundColor: this.infoColor,
             borderWidth: 2,
-            pointRadius: 0
+            pointRadius: 3
         },
         deaths: {
             label: 'Deaths',
-            data: [],
+            data: [] as Array<number>,
             fill: false,
-            borderColor: '',
-            backgroundColor: '',
+            borderColor: this.errorColor,
+            backgroundColor: this.errorColor,
             borderWidth: 2,
-            pointRadius: 0,
+            pointRadius: 3
         },
         recovered: {
             label: 'Recovered',
-            data: [],
+            data: [] as Array<number>,
             fill: false,
-            borderColor: '',
-            backgroundColor: '',
+            borderColor: this.primaryColor,
+            backgroundColor: this.primaryColor,
             borderWidth: 2,
-            pointRadius: 0,
+            pointRadius: 3
         }
     };
 
     mounted() {
-        console.log("test");
+        this.element = this.$el.querySelector('.js-canvas') as HTMLCanvasElement;
+        this.ctx = this.element.getContext('2d') as CanvasRenderingContext2D;
+        this.fetchStats();
     }
 
     plot() {
@@ -59,13 +67,25 @@ export default class Analytics extends Vue {
                     this.datasets.recovered,
                     this.datasets.deaths
                 ]
-            },
+            } as ChartData,
             options: {
                 responsive: true,
-                maintainAspectRatio: false,
+                title: {
+					display: true,
+					text: 'Overall Stats'
+				},
+                maintainAspectRatio: true,
                 legend: {
-                    display: false,
+                    display: true,
                 },
+                tooltips: {
+					mode: 'index',
+					intersect: false,
+				},
+				hover: {
+					mode: 'nearest',
+					intersect: true
+				},
                 elements: {
                     point: {
                         radius: 1,
@@ -75,6 +95,55 @@ export default class Analytics extends Vue {
                 }
             }
         });
+    }
+
+    async fetchStats() {
+
+        //trigger the topbar loader
+        this.$root.$emit('request-loading', true);
+
+        try {
+            const casesStats = await fb.casesStatsCollection.get();
+            const deathsStats = await fb.deathsStatsCollection.get();
+            const recoveredStats = await fb.recoveredStatsCollection.get();
+
+            if (casesStats) {
+                casesStats.forEach((doc) => {
+                    const { date, value } = doc.data();
+                    this.datasets.cases.data.push(value);
+
+
+                    const unixTimeStamp = new Date(date.seconds * 1000);
+                    const day = unixTimeStamp.getDate();
+                    const month = unixTimeStamp.toLocaleString('default', { month: 'short' });
+                    const year = unixTimeStamp.getFullYear();
+
+                    this.labels.push(`${day} ${month} ${year}`);
+                });
+            }
+
+            if (deathsStats) {
+                deathsStats.forEach((doc) => {
+                    const { date, value } = doc.data();
+                    this.datasets.deaths.data.push(value);
+                });
+            }
+
+            if (recoveredStats) {
+                recoveredStats.forEach((doc) => {
+                    const { date, value } = doc.data();
+                    this.datasets.recovered.data.push(value);
+                });
+            }
+
+            this.$root.$emit('request-loading', false);
+            this.plot();
+        } catch (error) {
+            if (error) {
+                console.log(error);
+                this.$root.$emit('request-loading', false);
+            }
+        }
     }
 }
 </script>
