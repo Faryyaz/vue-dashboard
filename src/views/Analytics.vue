b684d3939e230fd8cb65e9c6a1a59fa58aba2082
Faryyaz Dilmahomed <fdilmahomed@multiply.cloud>
2021-03-17 17:34:58 +0400

    [WIP] Analytics

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -2,7 +2,7 @@
     <v-card max-height="100%">
         <v-card-title>Analytics</v-card-title>
         <v-card-text>
-            <canvas id="canvas"></canvas>
+            <canvas class="js-canvas"></canvas>
         </v-card-text>
     </v-card>
 </template>
@@ -12,24 +12,69 @@ import { Chart, ChartData } from 'chart.js'
 
 @Component
 export default class Analytics extends Vue {
-    element = <HTMLCanvasElement> this.$el.querySelector('#canvas');
-    ctx =  <CanvasRenderingContext2D> this.element.getContext('2d');
+    element = this.$el.querySelector('.canvas') as HTMLCanvasElement;
+    ctx = this.element.getContext('2d') as CanvasRenderingContext2D;
     labels: Array<string> = [];
-    datasets: Array<string> = [];
+    datasets = {
+        cases: {
+            label: 'Cases',
+            data: [],
+            fill: false,
+            borderColor: '',
+            backgroundColor: '',
+            borderWidth: 2,
+            pointRadius: 0
+        },
+        deaths: {
+            label: 'Deaths',
+            data: [],
+            fill: false,
+            borderColor: '',
+            backgroundColor: '',
+            borderWidth: 2,
+            pointRadius: 0,
+        },
+        recovered: {
+            label: 'Recovered',
+            data: [],
+            fill: false,
+            borderColor: '',
+            backgroundColor: '',
+            borderWidth: 2,
+            pointRadius: 0,
+        }
+    };
 
     mounted() {
-
+        console.log("test");
     }
 
     plot() {
         new Chart(this.ctx, {
             type: 'line',
-            data: <ChartData> {
+            data: {
                 labels: this.labels,
-                datasets: this.datasets
+                datasets: [
+                    this.datasets.cases, 
+                    this.datasets.recovered,
+                    this.datasets.deaths
+                ]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                    display: false,
+                },
+                elements: {
+                    point: {
+                        radius: 1,
+                        hitRadius: 2,
+                        hoverRadius: 5
+                    }
+                }
             }
         });
     }
-
 }
 </script>
