b55ae5f2dbf0ae783ed5188305c3d3fa06236e28
Faryyaz <dilfary@hotmail.com>
2021-03-18 20:52:13 +0400

    Added config for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "@typescript-eslint/interface-name-prefix": [ "error", { "prefixWithI": "always" }],
+    '@typescript-eslint/no-this-alias': 'off'
   }
 }
