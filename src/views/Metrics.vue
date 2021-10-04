9d2088d08403849f7fe0f204e6555f5f47a4e417
Faryyaz Dilmahomed <fdilmahomed@multiply.cloud>
2021-10-04 12:18:24 +0400

    Added table and bug fixes

--- src/views/Metrics.vue
+++ src/views/Metrics.vue
@@ -1,32 +1,44 @@
 <template>
-    <div>
-        <grid-layout :layout.sync="layout"
-                 :col-num="12"
-                 :row-height="40"
-                 :responsive="true"
-                 :is-draggable="draggable"
-                 :is-resizable="resizable"
-                 :vertical-compact="true"
-                 :use-css-transforms="true"
-        >
-            <template v-for="item in layout">
-                <grid-item 
-                    :static="item.static"
-                    :x="item.x"
-                    :y="item.y"
-                    :w="item.w"
-                    :h="item.h"
-                    :i="item.i"
-                    :key="item.i"
-                    :preserveAspectRatio="item.preserveAspectRatio"
-                    @resized="resizeGraph">
-                    <template>
-                        <div :class="item.name" class="chart elevation-1"></div>
-                    </template>
-                </grid-item>
-            </template>
-        </grid-layout>
-    </div>
+    <grid-layout :layout.sync="layout"
+                :col-num="12"
+                :row-height="40"
+                :responsive="true"
+                :is-draggable="draggable"
+                :is-resizable="resizable"
+                :vertical-compact="true"
+                :use-css-transforms="true"
+    >
+        <template v-for="item in layout">
+            <grid-item 
+                :static="item.static"
+                :x="item.x"
+                :y="item.y"
+                :w="item.w"
+                :h="item.h"
+                :i="item.i"
+                :key="item.i"
+                drag-allow-from=".draggable-handle"
+                :preserveAspectRatio="item.preserveAspectRatio"
+                @resized="resizeGraph"
+                class="rounded elevation-1 white pa-1">
+                <template v-if="item.type === 'chart'">
+                    <div :class="item.name" class="chart rounded"></div>
+                    <v-icon small color="secondary lighten-4" class="draggable-handle">open_with</v-icon>
+                </template>
+                <template v-else-if="item.type === 'table'">
+                    <div class="grid-table">
+                        <v-data-table
+                            :headers="headers"
+                            :items="desserts"
+                            :items-per-page="5"
+                            class="mt-4"
+                        ></v-data-table>
+                        <v-icon small color="secondary lighten-4" class="draggable-handle">open_with</v-icon>
+                    </div>
+                </template>
+            </grid-item>
+        </template>
+    </grid-layout>
 </template>
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator'
@@ -41,10 +53,11 @@ import * as Plotly from 'plotly.js'
 })
 export default class Overview extends Vue {
     layout = [
-        {"x":0,"y":0,"w":5,"h":6,"i":"0","static":false,"name":"line-graph","preserveAspectRatio":false},
-        {"x":0,"y":0,"w":5,"h":6,"i":"1","static":false,"name":"pie-chart","preserveAspectRatio":false},
-        {"x":5,"y":6,"w":5,"h":6,"i":"2","static":false,"name":"bar-chart","preserveAspectRatio":false},
-        {"x":5,"y":6,"w":5,"h":6,"i":"3","static":false,"name":"mixed-chart","preserveAspectRatio":false}
+        {"x":0,"y":0,"w":5,"h":6,"i":"0","static":false, type: "chart", "name":"line-graph","preserveAspectRatio":false},
+        {"x":0,"y":0,"w":5,"h":6,"i":"1","static":false, type: "chart", "name":"pie-chart","preserveAspectRatio":false},
+        {"x":5,"y":6,"w":5,"h":6,"i":"2","static":false, type: "chart", "name":"bar-chart","preserveAspectRatio":false},
+        {"x":5,"y":6,"w":5,"h":6,"i":"3","static":false, type: "chart", "name":"mixed-chart","preserveAspectRatio":false},
+        {"x":0,"y":12,"w":10,"h":8,"i":"4","static":false, type: "table", "name":"table","preserveAspectRatio":false}
     ];
     draggable = true;
     resizable = true;
@@ -53,19 +66,122 @@ export default class Overview extends Vue {
         responsive: true
     };
 
+    headers = [
+        {
+          text: 'Dessert (100g serving)',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (%)', value: 'iron' },
+    ];
+
+    desserts = [
+        {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+        },
+        {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+        },
+        {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+        },
+        {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+        },
+        {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+        },
+        {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+        },
+        {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+        },
+        {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+        },
+        {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+        },
+        {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+        },
+    ];
+
     mounted() {
-        this.plotLineGraph();
-        this.plotPieChart();
-        this.plotBarChart();
-        this.plotMixedChart();
-        this.resizeGraph();
+        this.$nextTick(() => {
+            this.plotLineGraph();
+            this.plotPieChart();
+            this.plotBarChart();
+            this.plotMixedChart();
+            this.resizeGraph();
+        });
     }
 
     resizeGraph(): void {
 
         // trigger the window resize event
         // for plotly to resize graph dynamically
-        window.dispatchEvent(new Event('resize'));
+        // window.dispatchEvent(new Event('resize'));
+        const elements: Array<HTMLElement> = Array.from(document.querySelectorAll('.chart'));
+        elements.forEach((element: HTMLElement) => {
+            Plotly.Plots.resize(element);
+        });
 
     }
 
@@ -83,7 +199,14 @@ export default class Overview extends Vue {
         };
 
         const layout = { 
-            title: 'Line graph'
+            title: 'Line graph',
+            autosize: true,
+            margin: {
+                l: 30,
+                r: 30,
+                b: 30,
+                t: 50
+            },
         };
 
         const data: any = [trace1, trace2];
@@ -100,7 +223,14 @@ export default class Overview extends Vue {
         }];
 
         const layout = {
-            title: 'Pie'
+            title: 'Pie',
+            autosize: true,
+            margin: {
+                l: 30,
+                r: 30,
+                b: 30,
+                t: 50
+            },
         };
 
         const element: any = document.querySelector('.pie-chart');
@@ -118,7 +248,14 @@ export default class Overview extends Vue {
         ];
 
         const layout = {
-            title: 'Bar chart'
+            title: 'Bar chart',
+            autosize: true,
+            margin: {
+                l: 30,
+                r: 30,
+                b: 30,
+                t: 50
+            },
         };
 
         const element: any = document.querySelector('.bar-chart');
@@ -142,7 +279,14 @@ export default class Overview extends Vue {
         const data: any = [trace1, trace2];
 
         const layout = {
-            title: 'Mixed chart'
+            title: 'Mixed chart',
+            autosize: true,
+            margin: {
+                l: 30,
+                r: 30,
+                b: 30,
+                t: 50
+            },
         };
 
         const element: any = document.querySelector('.mixed-chart');
@@ -158,5 +302,18 @@ export default class Overview extends Vue {
         width: 100%;
         height: 100%;
         position: relative;
+    }
+
+    .draggable-handle {
+        position: absolute;
+        top: 4px;
+        left: 4px;
+    }
+
+    .grid-table {
+        overflow: auto;
+        position: relative;
+        width: 100%;
+        height: 100%;
     }
 </style>
